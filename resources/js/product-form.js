ebd18f5d990f7b27d8f0986f814bdb56adaf0339
my <tramypham2192@gmail.com>
2022-06-27 14:26:40 -0700

    task 6 - incomplete

---
+++ resources/js/product-form.js
@@ -0,0 +1,78 @@
+alert('Hello World!')
+
+// const addProductButton = document.getElementById("addProductButton")
+// addProductButton.addEventListener("click", addProduct_createCard_displayProduct)
+// function addProduct_createCard_displayProduct(){
+    // const products = []
+    // const product = 
+    // products.push(product)
+    // let cardContainer;
+    // createProductCard(product)
+    // displayProducts()
+// let form = document.getElementById("form")
+// form.onclick = function(e){
+//     e.preventDefault();
+//     var name = document.getElementById("productName").value;
+//     console.log(name);
+// }
+// }
+
+// var products = [{
+//     "name": "dddd",
+//     "description": "dddd",
+//     // "img": "",
+//     // "createdAt": ""
+// },
+// {
+//     "name": "dddd",
+//     "description": "dddd",
+//     // "img": "",
+//     // "createdAt": ""
+// }
+// ];
+// products.push({"name": "yy",
+// "description": "yy"
+// })
+// let cardContainer;
+// let createProductCard = (product) => {
+//     let card = document.createElement('div');
+//     card.className = 'card shadow cursor-pointer';
+
+//     let cardBody = document.createElement('div');
+//     cardBody.className = 'card-body';
+
+//     let productName = document.createElement('h5');
+//     productName.innerText = product.name;
+//     productName.className = 'card-productName';
+
+//     let productDescription = document.createElement('div');
+//     productDescription.innerText = product.description;
+//     productDescription.className = 'card-productDescription';
+
+//     // let productImg = document.createElement('div');
+//     // productImg.innerText = product.img;
+//     // productImg.className = 'card-img';
+
+//     // let productCreatedAt = document.createElement('div');
+//     // productCreatedAt.innerText = product.createdAt;
+//     // productCreatedAt.className = 'card-createdAt';
+
+//     cardBody.appendChild(productName);
+//     cardBody.appendChild(productDescription);
+//     // cardBody.appendChild(productImg);
+//     // cardBody.appendChild(productCreatedAt);
+//     card.appendChild(cardBody);
+//     cardContainer.appendChild(card);
+// }
+// let displayProducts = () => {
+//     if (cardContainer) {
+//         document.getElementById('card-container').replaceWith(cardContainer);
+//         return;
+//     }
+
+//     cardContainer = document.getElementById('card-container');
+//     products.forEach((product) => {
+//         createProductCard(product);
+//     });    
+// };
+
